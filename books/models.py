89e9118dc696a4c35e3d38d480894fbc6d939bd9
SaugatMgr <saugatmagar433@gmail.com>
2023-05-24 20:36:36 +0545

    added categories and tags to book, added str method to categories and tags to return their names

--- books/models.py
+++ books/models.py
@@ -4,9 +4,15 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=64)
 
+    def __str__(self):
+        return self.name
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
 
 
 class Book(models.Model):
@@ -16,6 +22,15 @@ class Book(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     book_cover = models.ImageField(upload_to='covers/', blank=True)
     copies_sold = models.PositiveBigIntegerField(default=0)
+
+    # 1 to Many relationship with Book
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+    )
+
+    # Many to Many relationship with Book
+    tag = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.title
