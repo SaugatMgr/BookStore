c253cd423f50117dd4d021d5096ccfff5d3a9cb0
SaugatMgr <saugatmagar433@gmail.com>
2023-05-24 20:01:55 +0545

    Category and Tag model

--- books/models.py
+++ books/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+
+class Category(models.Model):
+    category = models.CharField(max_length=64)
+
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=64)
+
+
 class Book(models.Model):
     title = models.CharField(max_length=64)
     description = models.TextField()
@@ -7,7 +16,6 @@ class Book(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     book_cover = models.ImageField(upload_to='covers/', blank=True)
     copies_sold = models.PositiveBigIntegerField(default=0)
-    
+
     def __str__(self):
         return self.title
-    
