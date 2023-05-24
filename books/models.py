10221a65531ad61b4371d0020d4bcbfb79354799
SaugatMgr <saugatmagar433@gmail.com>
2023-05-24 16:47:38 +0545

    Books model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    author = models.CharField(max_length=64)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    book_cover = models.ImageField(upload_to='covers/', blank=True)
+    
+    def __str__(self):
+        return self.title
+    
