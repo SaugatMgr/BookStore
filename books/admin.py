184d25eaccf2ee62cbb4d717355ee62f4b0cc733
SaugatMgr <saugatmagar433@gmail.com>
2023-05-26 18:38:21 +0545

    registered Contact Model to admin site

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Book, Genre, Tag
+from .models import Book, Genre, Tag, Contact, Comment
 
 admin.site.register(Book)
 admin.site.register(Genre)
 admin.site.register(Tag)
+admin.site.register(Contact)
+admin.site.register(Comment)
