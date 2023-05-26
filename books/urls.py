921726510aa032c3687ced39d846080514e28485
SaugatMgr <saugatmagar433@gmail.com>
2023-05-26 13:00:48 +0545

    url for about page view

--- books/urls.py
+++ books/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from .views import (
     HomePageView,
     BookDetailView,
+    AboutPageView,
 )
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("books/<int:pk>/", BookDetailView.as_view(), name="detail"),
+    path("books/about/", AboutPageView.as_view(), name="about"),
 ]
