cb469c94e6b312b8c0d3b3d83c190f9949a754a6
SaugatMgr <saugatmagar433@gmail.com>
2023-05-26 18:39:08 +0545

    contact page url

--- books/urls.py
+++ books/urls.py
@@ -4,10 +4,12 @@ from .views import (
     HomePageView,
     BookDetailView,
     AboutPageView,
+    ContactPageView,
 )
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("books/<int:pk>/", BookDetailView.as_view(), name="detail"),
     path("books/about/", AboutPageView.as_view(), name="about"),
+    path("books/contact/", ContactPageView.as_view(), name="contact"),
 ]
