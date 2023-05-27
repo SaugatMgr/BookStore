c3796c077b8e592eaf1e31fed85e0000da4f2314
SaugatMgr <saugatmagar433@gmail.com>
2023-05-27 12:50:03 +0545

    Review form

--- books/forms.py
+++ books/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Contact, NewsLetter
+from .models import Contact, NewsLetter, Review
 
 
 class ContactForm(forms.ModelForm):
@@ -13,3 +13,12 @@ class NewsLetterForm(forms.ModelForm):
     class Meta:
         model = NewsLetter
         fields = "__all__"
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = [
+            "name",
+            "email",
+            "review",
+        ]
