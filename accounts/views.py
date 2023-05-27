3dabcace1eb2f648df21472abf9f6aabdb0d3bef
SaugatMgr <saugatmagar433@gmail.com>
2023-05-27 22:18:49 +0545

    SignUp View using Custom SignUp form

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,9 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth.forms import UserCreationForm
 
+from .forms import SignUpForm
 
 class SignUpPageView(CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     template_name = "registration/signup.html"
     success_url = reverse_lazy("login")
