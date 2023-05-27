1a1ff98f7c42c486d67bfbcca1d907068a35476a
SaugatMgr <saugatmagar433@gmail.com>
2023-05-27 10:16:05 +0545

    comment to reviews

--- books/admin.py
+++ books/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Book, Genre, Tag, Contact, Comment, NewsLetter
+from .models import Book, Genre, Tag, Contact, Review, NewsLetter
 
 admin.site.register(Book)
 admin.site.register(Genre)
 admin.site.register(Tag)
 admin.site.register(Contact)
-admin.site.register(Comment)
+admin.site.register(Review)
 admin.site.register(NewsLetter)
