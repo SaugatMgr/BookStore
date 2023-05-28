a0a204375b6318a1c618453813074625120fd5fe
SaugatMgr <saugatmagar433@gmail.com>
2023-05-28 16:52:48 +0545

    override default auth login url and view and added logout url as well

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from .forms import LoginForm
+
 from .views import (
     SignUpPageView,
 )
 
 urlpatterns = [
     path("signup/", SignUpPageView.as_view(), name="signup"),
+    path("login/", auth_views.LoginView.as_view(
+        template_name="registration/login.html",
+        authentication_form=LoginForm),
+        name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
