7eddf73a4bc2f597acd98585d18db1372ec3e14a
SaugatMgr <saugatmagar433@gmail.com>
2023-05-28 22:54:18 +0545

    password change and password change done url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from .forms import LoginForm
+from .forms import (
+    LoginForm,
+    ChangePasswordForm,
+)
 
 from .views import (
     SignUpPageView,
@@ -14,4 +17,9 @@ urlpatterns = [
         authentication_form=LoginForm),
         name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("password-change/", auth_views.PasswordChangeView.as_view(
+        form_class=ChangePasswordForm),
+        name="password_change"),
+    path("password-change/done/", auth_views.PasswordChangeDoneView.as_view(),
+         name="password_change_done"),
 ]
