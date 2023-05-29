b550755d745f2f206938ec9cf3dae70a682c3bc4
SaugatMgr <saugatmagar433@gmail.com>
2023-05-29 14:42:58 +0545

    password reset urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,15 +11,27 @@ from .views import (
 )
 
 urlpatterns = [
+    # Signup
     path("signup/", SignUpPageView.as_view(), name="signup"),
+    # Login/Logout
     path("login/", auth_views.LoginView.as_view(
         template_name="registration/login.html",
         authentication_form=LoginForm),
         name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    # Password Change
     path("password-change/", auth_views.PasswordChangeView.as_view(
         form_class=ChangePasswordForm),
         name="password_change"),
     path("password-change/done/", auth_views.PasswordChangeDoneView.as_view(),
          name="password_change_done"),
+    # Password Reset
+    path("password-reset/", auth_views.PasswordResetView.as_view(),
+         name="password_reset"),
+    path("password-reset/done/", auth_views.PasswordResetDoneView.as_view(),
+         name="password_reset_done"),
+    path("password-reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(),
+         name="password_reset_confirm"),
+    path("password-reset/complete/", auth_views.PasswordResetCompleteView.as_view(),
+         name="password_reset_complete"),
 ]
