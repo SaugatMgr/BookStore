0055b4263092fec416c28ed30d0572a64fda93e2
SaugatMgr <saugatmagar433@gmail.com>
2023-07-09 19:30:01 +0545

    CRUD operations urls for book

--- books/urls.py
+++ books/urls.py
@@ -8,10 +8,18 @@ from .views import (
     NewsLetterView,
     ReviewView,
     BooksByAuthorView,
+    AddBookView,
+    AllBooksView,
+    UpdateBookView,
+    DeleteBookView,
 )
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path("books/add-book/", AddBookView.as_view(), name="add_book"),
+    path("books/all-books/", AllBooksView.as_view(), name="all_books"),
+    path("books/update-book/<slug:slug>/", UpdateBookView.as_view(), name="update_book"),
+    path("books/delete-book/<slug:slug>/", DeleteBookView.as_view(), name="delete_book"),
     path("books/detail/<slug:slug>/", BookDetailView.as_view(), name="book_detail"),
     path("books/about/", AboutPageView.as_view(), name="about"),
     path("books/contact/", ContactPageView.as_view(), name="contact"),
