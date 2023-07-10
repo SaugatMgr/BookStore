53ca7295f3dbcf69c2bab45e30945809b40c9746
SaugatMgr <saugatmagar433@gmail.com>
2023-07-10 10:56:36 +0545

    url for all products

--- books/urls.py
+++ books/urls.py
@@ -12,12 +12,14 @@ from .views import (
     AllBooksView,
     UpdateBookView,
     DeleteBookView,
+    AllProductsView,
 )
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("books/add-book/", AddBookView.as_view(), name="add_book"),
     path("books/all-books/", AllBooksView.as_view(), name="all_books"),
+    path("books/all-products/", AllProductsView.as_view(), name="all_products"),
     path("books/update-book/<slug:slug>/", UpdateBookView.as_view(), name="update_book"),
     path("books/delete-book/<slug:slug>/", DeleteBookView.as_view(), name="delete_book"),
     path("books/detail/<slug:slug>/", BookDetailView.as_view(), name="book_detail"),
