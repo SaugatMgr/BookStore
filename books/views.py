d356af7118a1e1a82184ec9bfaf48a96be30ed40
SaugatMgr <saugatmagar433@gmail.com>
2023-07-12 09:25:03 +0545

    implemented proper authentication and authorization mixin for user access and restriction for different access (CRUD operations on book) and others

--- books/views.py
+++ books/views.py
@@ -11,11 +11,21 @@ from django.views.generic import (
     TemplateView,
 )
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.http import JsonResponse
 
 from .forms import ContactForm, NewsLetterForm, ReviewForm, BookForm
 
 from .models import Book, Genre, Review
+
+# Listing Permission codenames
+
+# from django.contrib.auth.models import Permission
+# from django.contrib.contenttypes.models import ContentType
+
+# content_type = ContentType.objects.get_for_model(Book)
+# post_permission = Permission.objects.filter(content_type=content_type)
+# print([perm.codename for perm in post_permission])
 
 
 class HomePageView(ListView):
@@ -30,7 +40,7 @@ class HomePageView(ListView):
             copies_sold__gt=0).order_by("-copies_sold")
 
         context['best_selling_book'] = filter_object.first()
-        
+
         context['books_with_offer'] = filter_object[:5]
 
         context['popular_books_top'] = filter_object[:4]
@@ -50,7 +60,7 @@ class AboutPageView(TemplateView):
     template_name = 'about.html'
 
 
-class ContactPageView(View):
+class ContactPageView(LoginRequiredMixin, View):
     template_name = "contact.html"
 
     def get(self, request, *args, **kwargs):
@@ -79,7 +89,7 @@ class BookDetailView(DetailView):
     context_object_name = 'book'
 
 
-class NewsLetterView(View):
+class NewsLetterView(LoginRequiredMixin, View):
     def post(self, request, *args, **kwargs):
         ajax_format = request.headers.get("x-requested-with")
 
@@ -114,7 +124,7 @@ class NewsLetterView(View):
         )
 
 
-class ReviewView(View):
+class ReviewView(LoginRequiredMixin, View):
     def post(self, request, *args, **kwargs):
         form = ReviewForm(request.POST)
         book_id = request.POST["book"]
@@ -157,47 +167,53 @@ class BooksByAuthorView(ListView):
         return context
 
 
-class AddBookView(CreateView):
+class AddBookView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Book
     form_class = BookForm
     template_name = "book/add_book.html"
+    permission_required = "add_book"
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
 
-class AllBooksView(ListView):
+class AllBooksView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = Book
     template_name = "book/all_books.html"
     context_object_name = "books"
+    permission_required = "view_book"
 
 
-class UpdateBookView(UpdateView):
+class UpdateBookView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = Book
     form_class = BookForm
     template_name = "book/update_book.html"
+    permission_required = "change_book"
 
 
-class DeleteBookView(DeleteView):
+class DeleteBookView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = Book
     template_name = "book/delete_book.html"
     success_url = reverse_lazy("home")
+    permission_required = "delete_book"
 
 
 class AllProductsView(ListView):
     model = Book
     template_name = "all_products.html"
     context_object_name = "books"
-   
+
+
 class SearchView(ListView):
     model = Book
     template_name = "book/book_search_list.html"
     context_object_name = "book_list"
-    
+
     def get_queryset(self):
         query = self.request.GET.get("query")
-        book_list =  Book.objects.filter(
-            (Q(title__icontains=query) | Q(author__icontains=query) | Q(tag__name__icontains=query) | Q(genre__name__icontains=query) | Q(description__icontains=query))
-        ).order_by("-copies_sold") 
+        book_list = Book.objects.filter(
+            (Q(title__icontains=query) | Q(author__icontains=query) | Q(tag__name__icontains=query) | Q(
+                genre__name__icontains=query) | Q(description__icontains=query))
+        ).order_by("-copies_sold")
         return book_list
